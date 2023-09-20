b6200ebe51dd958b41396b6584416b433fff1d94
Shafwan Haidar <wanderergenshin86@gmail.com>
2023-09-20 22:18:06 +0700

    commit 1

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// Toogle class active
+const navbarNav = document.querySelector ('.navbar-nav');
+
+
+// ketika hamburger menu di klik
+document.querySelector('#hamburger-menu').
+oneclick = () => {
+    navbarNav.classList.toogle('active');
+};
+
+// klik di luar side bar untuk menghilangkan nav
+const hamburger = document.querySelector('hamburger-menu');
+
+document.addEventListener('click' function(e) {
+    if(hamburger.contains(e.target)) && !navbarNav.contains(e.target)) {
+        navbarNav.classList.remove('active')
+    }
+});
